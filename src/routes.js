89f78297a4d7c2f64ba4031b1514af2ae9069926
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-05-23 15:05:17 -0300

    feat: criação de estrutura de rotas

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+export const routes = [
+  {
+    method: 'GET',
+    path: 'tasks',
+    handler: (req, res) => {
+      return null
+    },
+  },
+  {
+    method: 'POST',
+    path: 'tasks',
+    handler: (req, res) => {
+      return null
+    },
+  },
+  {
+    method: 'PUT',
+    path: 'tasks/:id',
+    handler: (req, res) => {
+      return null
+    },
+  },
+  {
+    method: 'PATCH',
+    path: 'task/:id/complete',
+    handler: (req, res) => {
+      return null
+    },
+  },
+  {
+    method: 'DELETE',
+    path: 'task/:id',
+    handler: (req, res) => {
+      return null
+    },
+  }
+]
